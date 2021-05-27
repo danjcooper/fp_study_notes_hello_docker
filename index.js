b22a8060e8fe5598b5f0566a9349fddc52258916
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-05-27 21:42:48 +0100

    Added a name length calculator function

--- index.js
+++ index.js
@@ -6,9 +6,25 @@ const ui = require('readline').createInterface({
 ui.question('Hi there! What is your name?\n\n', input => {
     const message = sayHello(input)
     console.log(message)
+    const messageTwo = sayNameLength(input)
+    console.log(messageTwo)
     ui.close();
 })
 
-function sayHello(name){
+
+
+function sayHello(name) {
     return `\tHello, ${name}!\n`
+}
+
+function sayNameLength(name) {
+    // The name length will count spaces as a letter, this calculates the spaces and removes them from the total.
+    let count = 0
+    for (let i = 0; i < name.length; i++) {
+        if (name[i] === ' ') {
+            count++
+        }
+    }
+
+    return `Your name is ${name.length - count} letters long.`
 }
